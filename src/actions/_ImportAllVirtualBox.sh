0e250f144af2a0b4078efa20a8e455c6d8841808
Edwin Kofler <edwin@kofler.dev>
2022-04-03 18:45:33 -0700

    Update: April 03, 2022 (18:45)

---
+++ src/actions/_ImportAllVirtualBox.sh
@@ -0,0 +1,49 @@
+# shellcheck shell=bash
+
+# Name:
+# VirtualBox Import
+#
+# Description:
+# Registers all VirtualBox virtual machine. Optionally
+# enables unregistering all virtual machines
+
+shopt -s nullglob
+
+action() {
+  local flag_unregister='no'
+
+  for arg; do case $arg in
+    --help|-h) printf '%s\n' "Usage: $0 [-h|--help] [--unregister] (add is default)" ;;
+    --unregister) flag_unregister='yes'
+  esac done
+
+  if [ "$flag_unregister" = 'yes' ]; then
+    while IFS= read -r line; do
+      printf '%s\n' "Removing '${line}'"
+      local uuid="${line%\}}"
+      uuid=${uuid##*\{}
+      VBoxManage unregistervm "$uuid"
+    done < <(VBoxManage list vms)
+    return
+  fi
+
+  local virtualbox_dir="/storage/vault/rodinia/Virtual_Machines"
+
+  # Add all
+  for group_dir in "$virtualbox_dir"/*/; do
+    register "$group_dir"
+    local group_name="${group_dir%/}"; group_name=${group_name##*/}
+    for vm_dir in "$group_dir"*/; do
+      register "$vm_dir"
+    done
+  done
+}
+
+register() {
+  local dir="$1"
+
+  for file in "$dir"/*.vbox; do
+    printf '%s\n' "Adding '$file'"
+    VBoxManage registervm "$file"
+  done
+}
---
+++ src/actions/_minecraft-sync.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+# Name:
+# Sync .minecraft
+#
+# Description:
+# Syncs subdirectories in minecraft, like 'resourcepacks', 'saves', 'screenshots',
+# for all major launchers. Use at your own risk!
+
+remove() {
+	if [ -z "$1" ]; then
+		printf '%s\n' "Error: remove: Argument 1, file not found. Exiting" >&2
+		exit 1
+	fi
+
+	if command -v trash-put &>/dev/null; then
+		trash-put "$1"
+	else
+		mkdir -p "$base/trash-directory"
+		mv "$1" "$base/trash-directory/$1-$RANDOM"
+	fi
+}
+
+shopt -s nullglob dotglob
+
+base="$HOME/Docs/Games/Minecraft/common-dot-minecraft"
+dataDir="${XDG_DATA_HOME:-~/.local/share}"
+configDir="${XDG_CONFIG_HOME:-~/.config}"
+folders=(
+	~/.minecraft
+	"$dataDir"/multimc/instances/*/.minecraft
+	"$configDir"/hmcl/.minecraft
+)
+
+# sync common files
+for mcFolder in "${folders[@]}"; do
+	echo "SYNCING COMMON FILES: $mcFolder"
+	# files=(optionsLC.txt optionsof.txt optionsshaders.txt options.txt servers.dat servers.dat_old)
+	files=(servers.dat)
+
+	for file in "${files[@]}"; do
+
+		# unlink "$mcFolder/$file"
+
+		if [ ! -L "$mcFolder/$file" ]; then
+			echo "     -> Linking '$mcFolder/$file'"
+
+			[ -e "$mcFolder/$file" ] && {
+				cp "$mcFolder/$file" "$base/files/$file"
+				remove "$mcFolder/$file"
+			}
+
+			ln -sT "$base/files/$file" "$mcFolder/$file"
+		fi
+	done
+done
+
+echo ---
+
+# sync common folders
+for mcFolder in "${folders[@]}"; do
+	echo "SYNCING COMMON DIRS: $mcFolder"
+	declare -a subfolders=(resourcepacks shaderpacks saves screenshots)
+
+	for subfolder in "${subfolders[@]}"; do
+		mkdir -p "$base/$subfolder"
+		if [ -d "$mcFolder/$subfolder" ] && [ ! -L "$mcFolder/$subfolder" ]; then
+			echo "     -> Symlinking '$mcFolder/$subfolder'"
+
+			for file in "$mcFolder/$subfolder"/*; do
+				cp -r "$file" "$base/$subfolder"
+				remove "$file"
+			done
+			rmdir "$mcFolder/$subfolder"
+		fi
+
+		ln -sT "$base/$subfolder" "$mcFolder/$subfolder"
+	done
+done
